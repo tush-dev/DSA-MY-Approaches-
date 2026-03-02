6091effb3149ab705158e3abdce1db92f40ac222
Tushar Panwar <panwartushar23@gmail.com>
2026-03-02 23:31:50 +0530

    Time: 74 ms (77.24%), Space: 56.3 MB (40.33%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int slidingWindow (vector<int>& nums, int k)
+    { // total count of sub-arrays havign k distinct elements
+        unordered_map<int,int> mp;
+        int i=0;
+        int j=0;
+        int count=0;
+        int n =nums.size();
+        while(j<n)
+        {
+            mp[nums[j]]++ ;
+            while(mp.size()>k)      //shrink the window 
+            {
+                mp[nums[i]]--;
+                if(mp[nums[i]] ==0){
+                mp.erase(nums[i]);}
+                i++;
+            }
+            count += (j-i+1) ; //ending at j
+            j++;
+        }
+        return count;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return slidingWindow(nums,k)-slidingWindow(nums,k-1) ;
+    }
+};
